5a5914405376c21baafbf35d5565b76858652cf6
Narayan <totallacc@gmail.com>
2016-03-06 03:01:10 +0000

    update files

--- main.c
+++ main.c
@@ -14,6 +14,10 @@ int main (int argc, char** argv){
     printf ("The inverted string is: %s\n", string);
     string = up_sort (string);
     printf ("The up sorted string is: %s\n", string);
+    string = insertion_up_sort (string);
+    printf ("The insertion up sorted string is: %s\n", string);
+    string = hhh_up_sort (string);
+    printf ("The hhh up sorted string is: %s\n", string);
     free (string);
     return 0;
 }
--- text.c
+++ text.c
@@ -22,13 +22,13 @@ char* reverse_string (char* string){
 
 /*****Sort Up*****/
 char* up_sort (char* string){
-  char* tmp_string = NULL;
+  //char* tmp_string = NULL;
   char tmp_char_lower, tmp_char;
   short unsigned int i = 0, j;
   size_t len = 0;
 
   len = strlen (string);
-  tmp_string = mem_alloc (BUFFER);
+  //tmp_string = mem_alloc (BUFFER);
 
   tmp_char_lower = string [i];
   for (j = 0; j < len; j++){
@@ -45,6 +45,38 @@ char* up_sort (char* string){
   //i = j;                                //recomeça a pesquisa pelo char mais pequeno a partir da posição anterior + 1 já que a posição anterior já tem o char mais pequeno encontrado em toda a string
   }
   string [len + 1] = '\0';
-  free (string);
-  return tmp_string;
+  //free (string);
+  return string;
+}
+
+char* insertion_up_sort (char* string){
+  short unsigned int y = 0, index_min = 0, x;
+  size_t len = 0;
+  char tmp_char;
+
+  len = strlen (string);
+  for (x = 0; x < len; x++){
+    index_min = x;
+    for (y = x; y < len; y++){
+      if (string [index_min] > string [y])
+        index_min = y;
+    }
+    tmp_char = string [x];
+    string [x] = string [index_min];
+    string [index_min] = tmp_char;
+  }
+  return string;
+}
+
+char* hhh_up_sort (char* string){
+  size_t length = strlen (string), check = length / 2;
+  char aux;
+  unsigned int i;
+
+  for (i = 0; i < check; i++){
+    aux = string [i];
+    string [i] = string [length - i];
+    string [length - i] = aux;
+  }
+  return string;
 }
