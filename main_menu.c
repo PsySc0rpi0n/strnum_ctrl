23549994c0de29000f389d66c6b311a33f0b4769
José Carlos Ferreira <totallacc@gmail.com>
2016-03-08 16:41:23 +0000

    Create main_menu.c

---
+++ main_menu.c
@@ -0,0 +1,28 @@
+void menu_Main (void){
+  printf ("Main Menu\n");
+  printf ("\tSelect an Option:\n");
+  printf ("\t\t[1] - Strings\n");
+  printf ("\t\t[2] - Numbers\n");
+  printf ("\t\t[0] - Exit\n");
+  puts ("");
+}
+
+void menu_Strings (void){
+  printf ("Menu Strings\n");
+  printf ("\t[1] - Reverse String\n");
+  printf ("\t[2] - Up Sort String\n");
+  printf ("\t[3] - Down Sort String\n");
+  printf ("\t[4] - Compare Strings\n");
+  printf ("\t[0] - Back\n");
+  puts ("");
+}
+
+void menu_Numbers (void){
+  printf ("Menu Numbers\n");
+  printf ("\t[1] - Reverse Number\n");
+  printf ("\t[2] - Up Sort Number\n");
+  printf ("\t[3] - Down Sort Number\n");
+  printf ("\t[4] - Compare Numbers\n");
+  printf ("\t[0] - Back\n");
+  puts ("");
+}
