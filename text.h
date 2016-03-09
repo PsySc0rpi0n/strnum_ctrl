72fa7772dccbec7ffc5cc7d757518f08b7af220f
José Carlos Ferreira <totallacc@gmail.com>
2016-03-09 22:53:05 +0000

    Update text.h

--- text.h
+++ text.h
@@ -1,9 +1,10 @@
 #pragma once
-
+ 
 #include <string.h>
-
+ 
 #define BUFFER 256
-
+#define CLEAR_INPUT while (getchar () != '\n') /*void*/
+ 
 char* reverse_string          (char* string);
 unsigned short int string_cmp (char* str_a, char* str_b);
 char* insertion_up_sort       (char* string);
