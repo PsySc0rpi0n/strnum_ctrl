3d6813bd132d12fab208f27f1c5e45ca6f859503
José Carlos Ferreira <totallacc@gmail.com>
2016-03-09 22:49:42 +0000

    Update mem.c

--- mem.c
+++ mem.c
@@ -1,19 +1,19 @@
 #include "mem.h"
-
+ 
 /*****Memory Control*****/
 int mem_error = MEM_ERROR_OK;
-
+ 
 void* mem_alloc (const int size, unsigned short int dbm){
     void* result = NULL;
-
+ 
     if ((result = malloc(size)) == NULL)
       mem_error = MEM_ERROR_NO_MEMORY;
     else if (dbm == 1)
       printf ("Allocated memory: %p\n", result);
-
+ 
     return result;
 }
-
+ 
 void mem_free (void* ptr, unsigned short int dbm){
   if (!ptr)
     return;
@@ -21,11 +21,11 @@ void mem_free (void* ptr, unsigned short int dbm){
     printf ("Free'ed memory: %p\n", ptr);
   free (ptr);
 }
-
+ 
 int mem_get_error(void){
     return mem_error;
 }
-
+ 
 int mem_clear_error(void){
     return mem_error = MEM_ERROR_OK;
 }
