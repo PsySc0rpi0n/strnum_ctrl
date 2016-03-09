95292d5a5d5f60bb8af3a391a9b34c600202a760
José Carlos Ferreira <totallacc@gmail.com>
2016-03-09 22:51:38 +0000

    Update mem.h

--- mem.h
+++ mem.h
@@ -1,13 +1,13 @@
 #pragma once
-
+ 
 #include <stdio.h>
 #include <stdlib.h>
-
+ 
 #define MEM_ERROR_OK             0
 #define MEM_ERROR_NO_MEMORY     -1
-
+ 
 void* mem_alloc(const int size, unsigned short int dbm);
 void mem_free (void* ptr, unsigned short int dbm);
-
+ 
 int   mem_get_error(void);
 int   mem_clear_error(void);
